417b8ce9f1ecf0bc1eac05c5ed3f6b76b52e50fc
KecseKaroly <83023819+KecseKaroly@users.noreply.github.com>
2023-02-16 17:36:55 +0100

    Optimalizálst eager loading használata megjelenítéshez is

--- Vizsgaztato/app/Http/Controllers/TestsGroupsController.php
+++ Vizsgaztato/app/Http/Controllers/TestsGroupsController.php
@@ -8,33 +8,33 @@ use Illuminate\Http\Request;
 
 class TestsGroupsController extends Controller
 {
-    public function store($selectedResults, $testId) {
-        foreach($selectedResults as $selectedResult) {
-            if( !(TestsGroups::where(['test_id'=>$testId, 'group_id'=>$selectedResult['id']]))->exists())
-            {
-                $test_group = new TestsGroups();
-                $test_group->test_id = $testId;
-                $test_group->group_id = $selectedResult['id'];
-                $test_group->save();
-            }
+    public function store($selectedResults, $testId)
+    {
+        $test = test::find($testId);
+        foreach ($selectedResults as $selectedResult) {
+            if (!$test->groups->contains($selectedResult['id']))
+                $test->groups->attach($selectedResult);
         }
     }
 
-    public function show($testId) {
+    public function show($testId)
+    {
         $test_groups = DB::table('tests_groups')
-        ->join('groups', 'groups.id', '=', 'tests_groups.group_id')
-        ->select('groups.*', 'tests_groups.id', 'tests_groups.enabled_from', 'tests_groups.enabled_until')
-        ->where('tests_groups.test_id', $testId)
-        ->get();
-        return view('test.groups.show', ['test_groups'=>$test_groups, 'testId'=>$testId]);
+            ->join('groups', 'groups.id', '=', 'tests_groups.group_id')
+            ->select('groups.*', 'tests_groups.id', 'tests_groups.enabled_from', 'tests_groups.enabled_until')
+            ->where('tests_groups.test_id', $testId)
+            ->get();
+        return view('test.groups.show', ['test_groups' => $test_groups, 'testId' => $testId]);
     }
 
-    public function delete(Request $request) {
+    public function delete(Request $request)
+    {
         $test_group = TestsGroups::find($request->test_group_id);
         $test_group->delete();
     }
 
-    public function update(Request $request) {
+    public function update(Request $request)
+    {
         $test_group = TestsGroups::find($request->test_group_id);
         $test_group->enabled_from = $request->enabled_from;
         $test_group->enabled_until = $request->enabled_until;
