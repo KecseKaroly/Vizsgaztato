6868c6a6123c674825f30a83339e407ffe56940e
KecseKaroly <83023819+KecseKaroly@users.noreply.github.com>
2023-02-28 12:52:50 +0100

    Middleware auth csoport a web.php-ban

--- Vizsgaztato/routes/web.php
+++ Vizsgaztato/routes/web.php
@@ -21,33 +21,35 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::get('/', function () {
-    return view('layouts.app');
-});
-Route::get('/home', function () {
-    return view('layouts.app');
-});
-Route::get('/attempt/{attempt_id}/result', [TestController::class, 'show'])->middleware('auth')->name('testAttempts.show');
-Route::delete('/attempt/delete', [TestAttemptController::class, 'destroy'])->middleware('auth')->name('testAttempts.delete');
-Route::get('result/{tid}/{gid}/', [TestAttemptController::class, 'index'])->middleware('auth')->name('testAttempts.index');
-Route::get('result/{tid}/', [TestAttemptController::class, 'indexAllGroups'])->middleware('auth')->name('testAttempts.index.all');
-Route::get('/test/{id}/info/', [TestController::class, 'testInfo'])->middleware('auth')->name('checkTestInfo');
-Route::get('/test/{id}/group/{gid}/show', [TestController::class, 'show'])->middleware('auth')->name('test.show');
-Route::put('/test/group/update', [TestsGroupsController::class, 'update'])->middleware('auth')->name('updateTestGroup');
-Route::resource('test', TestController::class)->middleware('auth')->only(['index', 'create', 'edit', 'update', 'destroy']);
-
-Route::get('/groups/invites', [GroupInvController::class, 'index'])->middleware('auth')->name('inv_requests');
-
-Route::post('/join_request/submit', [GroupJoinRequestController::class, 'SubmitRequest'])->middleware('auth')->name('JoinRequestSubmit');
-Route::post('/join_request/accept', [GroupJoinRequestController::class, 'AcceptRequest'])->middleware('auth')->name('acceptGroupJoinRequest');
-Route::delete('/join_request/decline', [GroupJoinRequestController::class, 'RejectRequest'])->middleware('auth')->name('declineGroupJoinRequest');
-Route::get('/group/{id}/join_requests', [GroupJoinRequestController::class, 'index'])->middleware('auth')->name('join_requests');
-
-Route::post('/inv_request/accept', [GroupInvController::class, 'AcceptRequest'])->middleware('auth')->name('declineGroupInvRequest');
-Route::delete('/inv_request/decline', [GroupInvController::class, 'RejectRequest'])->middleware('auth')->name('declineGroupInvRequest');
-
-Route::delete('/group/user/{id}/remove', [GroupsUsersController::class, 'destroy'])->middleware('auth')->name('deleteUserFromGroup');
-Route::delete('/group/user/{id}', [GroupsUsersController::class, 'leave'])->middleware('auth')->name('leaveUserFromGroup');
-Route::resource('groups', GroupController::class)->middleware('auth');
-
 Auth::routes();
+Route::middleware(['auth'])->group(function() {
+
+    Route::get('/', function () {
+        return view('layouts.app');
+    });
+    Route::get('/home', function () {
+        return view('layouts.home');
+    })->name('home');
+    Route::get('/attempt/{attempt_id}/result', [TestAttemptController::class, 'show'])->name('testAttempts.show');
+    Route::delete('/attempt/delete', [TestAttemptController::class, 'destroy'])->name('testAttempts.delete');
+    Route::get('result/{tid}/{gid}/', [TestAttemptController::class, 'index'])->name('testAttempts.index');
+    Route::get('result/{tid}/', [TestAttemptController::class, 'indexAllGroups'])->name('testAttempts.index.all');
+    Route::get('/test/{id}/info/', [TestController::class, 'testInfo'])->name('checkTestInfo');
+    Route::get('/test/{id}/group/{gid}/show', [TestController::class, 'show'])->name('test.show');
+    Route::put('/test/group/update', [TestsGroupsController::class, 'update'])->name('updateTestGroup');
+    Route::resource('test', TestController::class)->only(['index', 'create', 'edit', 'update', 'destroy']);
+
+    Route::get('/groups/invites', [GroupInvController::class, 'index'])->name('inv_requests');
+
+    Route::post('/join_request/submit', [GroupJoinRequestController::class, 'SubmitRequest'])->name('JoinRequestSubmit');
+    Route::post('/join_request/accept', [GroupJoinRequestController::class, 'AcceptRequest'])->name('acceptGroupJoinRequest');
+    Route::delete('/join_request/decline', [GroupJoinRequestController::class, 'RejectRequest'])->name('declineGroupJoinRequest');
+    Route::get('/group/{id}/join_requests', [GroupJoinRequestController::class, 'index'])->name('join_requests');
+
+    Route::post('/inv_request/accept', [GroupInvController::class, 'AcceptRequest'])->name('declineGroupInvRequest');
+    Route::delete('/inv_request/decline', [GroupInvController::class, 'RejectRequest'])->name('declineGroupInvRequest');
+
+    Route::delete('/group/user/{id}/remove', [GroupsUsersController::class, 'destroy'])->name('deleteUserFromGroup');
+    Route::delete('/group/user/{id}', [GroupsUsersController::class, 'leave'])->name('leaveUserFromGroup');
+    Route::resource('groups', GroupController::class);
+});
