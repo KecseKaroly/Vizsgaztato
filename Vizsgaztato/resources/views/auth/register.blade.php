30ee08e6d3f556c71d44a97cf2cf0e82937e2799
KecseKaroly <83023819+KecseKaroly@users.noreply.github.com>
2023-03-23 11:06:54 +0100

    Felhasználási feltétel szövegének felvitele

--- Vizsgaztato/resources/views/auth/register.blade.php
+++ Vizsgaztato/resources/views/auth/register.blade.php
@@ -98,7 +98,7 @@
                         <input type="checkbox" unchecked class="focus:ring-purple-600 rounded-lg text-purple-500"
                                id="acceptTOS" name="acceptTOS">
                         <label class="text-gray-500 font-bold" for="acceptTOS">
-                            Elfogadom a <button data-modal-target="ToSModal"
+                            Elfogadom a <button type="button" data-modal-target="ToSModal"
                                                 data-modal-toggle="ToSModal"
                                                 >felhasználási feltételeket</button>*
                         </label>
@@ -128,15 +128,28 @@
                     <i class="fa-solid fa-xmark fa-2xl"></i>
                     <span class="sr-only">Bezárás</span>
                 </button>
-                <div class="px-16 py-8">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet debitis deserunt harum magnam, provident repudiandae tenetur? Architecto assumenda autem enim fugit labore maxime nemo, neque, nihil odio perferendis quod repellendus?
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, aperiam, commodi impedit incidunt ipsa natus nobis quod reiciendis repellendus sapiente sequi sit suscipit temporibus voluptas voluptatem. Magnam repellat voluptas voluptatem.
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab atque id ipsam sed. A ad adipisci dolorum eius exercitationem fuga id, in molestias mollitia provident quae quas saepe totam veritatis!
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias architecto, atque consequatur cupiditate dignissimos dolor eaque eos facere id, ipsa iure maiores natus nisi obcaecati placeat, quis saepe? Repudiandae!
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi ea eos molestias quibusdam quis quo, totam vel voluptas voluptatem. Ad aliquid dignissimos molestias mollitia, nisi officia optio repellat sed!
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam consequatur, impedit necessitatibus possimus repellat sapiente tempora. Adipisci aliquam autem commodi deleniti, dolores ducimus fugit illum maxime, quam, quas quos similique.
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem corporis, cumque fugiat iste magni neque nihil pariatur quaerat, quis quos reprehenderit saepe voluptates voluptatum. Consequuntur deleniti neque nesciunt quaerat rerum!
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa fuga fugit nam non nulla! Alias aspernatur beatae ea eos itaque iure minima modi neque nostrum ratione tempora, tempore unde ut!
+                <div class="px-16 py-8 prose h-72 overflow-y-auto bg-red-100 mx-auto">
+                    <ol>
+                        <li>
+                                Tudomásul veszem, hogy járulékos kötelezettségeként köteles vagyok az Eszterházy Károly Katolikus Egyetem (a továbbiakban: Egyetem) gazdasági érdekeinek védelmére, továbbá a munkám során tudomásomra jutott</li>
+                            <ul>
+                                <li>	üzleti titkot (a gazdasági tevékenységhez kapcsolódó minden olyan tényt, információt, megoldást vagy adatot, amelynek nyilvánosságra hozatala, illetéktelenek által történő megszerzése vagy felhasználása a jogosult jogszerű pénzügyi, gazdasági vagy piaci érdekeit sértené vagy veszélyeztetné), illetve</li>
+                                <li>	a kutatási tevékenységhez kapcsolódó minden olyan témát, információt, megoldást, eljárási módszert, adatot, amelynek titokban maradásához az Egyetemnek méltányolható érdeke fűződik, valamint</li>
+                                <li>	a munkavégzés során tudomásomra jutott személyes adatokat,</li>
+                                <li>	az Egyetemre, illetve a tevékenységére vonatkozó alapvető fontosságú információkat megőrizni.</li>
+                            </ul>
+                        <li>	Kötelezettséget vállalok arra, hogy az 1. pontban rögzítetteken túlmenően sem közlök illetéktelen személlyel olyan adatot, amely a köztem és az Egyetem között fennálló jogviszony során jutott a tudomásomra, és amelynek közlése az Egyetem vagy más személy számára hátrányos következménnyel járna.</li>
+                        <li>	Tudomásul veszem, hogy titoktartási kötelezettségem nem terjed ki a közérdekű adatok nyilvánosságára és a közérdekből nyilvános adatra vonatkozó, jogszabály vagy hatóság által előírt adatszolgáltatási és tájékoztatási kötelezettségre.</li>
+                        <li>	Tudomásul veszem, hogy titoktartási kötelezettségem a munkaviszonyom megszűnését követően is fennáll, erről szóló külön megállapodás nélkül is.</li>
+                        <li>	Tudomásul veszem, hogy amennyiben a titoktartási kötelezettségemet vétkesen, vagy nekem felróható okból megszegem, akkor az Egyetem a kötelezettségszegésből eredő károk megtérítését követelheti – illetve annak jogi úton érvényt szerez – az okozott kár értékének függvényében.</li>
+                        <li>	Tudomásul veszem, hogy a kutatási eredmények tulajdonjoga, valamint hasznosíthatósága vonatkozásában a vonatkozó jogszabályokat, valamint az Egyetem Szellemi tulajdon kezelési szabályzatát a közöttünk fennálló munkaviszony időtartama alatt magamra nézve kötelezőnek ismerem el.</li>
+                        <li>	Tudomásul veszem, hogy a korábban részletezett kutatási program során a titoktartás tárgykörébe tartozó kérdésekben a szakmai irányítóval köteles vagyok egyeztetni.</li>
+                        <li>	Eltérő megállapodás hiányában kötelezettséget vállalok arra, hogy az Egyetemmel fennálló jogviszonyom megszűnését követő öt munkanapon belül a kutatással kapcsolatos információkat, eredményeket tartalmazó dokumentációkat az Egyetem részére visszaszolgáltatom.</li>
+                        <li>	Tudomásul veszem, hogy a munkaköri feladataim ellátása során tudomásomra jutott személyes adatokat kizárólag az adatkezelő utasításai szerint kezelem, a lehető legteljesebb adatbiztonság érdekében meghozom a szükséges intézkedéseket, segítem az adatkezelőt a hatóság felé fennálló kötelezettségeinek a teljesítésében.</li>
+                        <li>	Tájékoztatom a munkáltatói jogkör gyakorlóját, ha annak valamely utasítása adatvédelmi rendelkezéseket sért.</li>
+                        <li>	Köteles vagyok arra, hogy a munkaviszonyom megszűnését követően az adatkezelő döntése alapján minden személyes adatot törlök vagy visszajuttatok az adatkezelőnek, és törlöm a meglévő másolatokat, kivéve, ha az uniós vagy a tagállami jog a személyes adatok tárolását írja elő.</li>
+                        <li>	Tudomásul veszem, hogy az adatkezelő épületeiben a GDPR 6. cikk (1) bekezdés f) pontja alapján vagyonvédelmi és ellenőrzési célból elektronikus megfigyelőrendszert (kamerákat) üzemeltet.</li>
+                    </ol>
                 </div>
                 <div class="flex items-center p-6 justify-evenly border-t-2">
                     <button data-modal-hide="ToSModal" type="button"
